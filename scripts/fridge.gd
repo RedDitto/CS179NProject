8e01fcc330b1065efe07d10332bee14328ab8ea8
asori047 <asori047@ucr.edu>
2024-05-09 20:21:17 -0700

    Got pathfinding to work with fridge in room_1
    Room scene now has a navigation region, the tilemap of a room is a child of the navigation region which is used for enemies to go around obstacles while chasing

--- scripts/fridge.gd
+++ scripts/fridge.gd
@@ -1,22 +1,38 @@
 extends CharacterBody2D
 
-var speed = 50
+var speed = 60
+var acceleration = 7
 var player_chase = false
 var player = null
 
+@onready var navigation_agent: NavigationAgent2D = $Navigation/NavigationAgent2D
 
 func _physics_process(delta):
 	if player_chase:
 		#position += (player.position - position) / speed
 		
-		var direction = (player.position - position).normalized()
+		#var direction = (player.position - position).normalized()
+		#var desired_velocity = direction * speed
+		#var steering = (desired_velocity - velocity) * delta * 2.5
+		#velocity += steering
+		#move_and_slide()
+		
+		var direction = Vector2.ZERO
+		navigation_agent.target_position = player.global_position
+		
+		
+		direction = navigation_agent.get_next_path_position() - global_position
+		direction = direction.normalized()
+		
+		velocity = velocity.lerp(direction * speed, acceleration * delta)
+		
 		var desired_velocity = direction * speed
 		var steering = (desired_velocity - velocity) * delta * 2.5
 		velocity += steering
+		
+		
 		move_and_slide()
-		
 		$AnimatedSprite2D.play("walk")
-		
 		#move_and_collide(Vector2(0,0))
 	else:
 		$AnimatedSprite2D.play("fridge_idle")
@@ -30,9 +46,15 @@ func _on_detection_area_body_entered(body):
 
 func _on_detection_area_body_exited(body):
 	if body.is_in_group("Player"):
-		player = false
+		player = null
 		player_chase = false
 	
 func enemy():
 	pass
 
+
+
+func _on_timer_timeout():
+	if player != null:
+		navigation_agent.target_position = player.global_position
+	
