a930b527cdbf9c6bb6341ebad7262a48c9dd0404
Manzana <73004527+ManzanaNaranja@users.noreply.github.com>
2024-05-29 15:03:44 -0700

    added limit to spawner

--- spawner.gd
+++ spawner.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 const fridgePATH = preload('res://Scenes/fridge.tscn')
+var limit = 3
+var count = 0
 func spawn():
 	pass
 
@@ -11,6 +13,9 @@ func _on_collision_shape_2d_child_entered_tree(node):
 
 func _on_detection_area_body_entered(body):
 	if(body.has_method("banana")):
-		var aaa = fridgePATH.instantiate()
-		aaa.position = Vector2(position.x, position.y)
-		get_parent().add_child(aaa)
+		if count < limit:
+			var aaa = fridgePATH.instantiate()
+			aaa.position = Vector2(position.x, position.y)
+			get_parent().add_child(aaa)
+			count = count + 1
+			
