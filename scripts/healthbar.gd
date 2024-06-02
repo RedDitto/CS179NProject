aa34039a315e1917cdbb7b83b6833be3a701d8f0
Manzana <73004527+ManzanaNaranja@users.noreply.github.com>
2024-06-02 14:47:29 -0700

    new boss fight stuff

--- scripts/healthbar.gd
+++ scripts/healthbar.gd
@@ -10,3 +10,4 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	value = get_parent().health
+	max_value = get_parent().max_health
