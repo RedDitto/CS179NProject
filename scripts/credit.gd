87524c9deebcf0928d1ed9044ee41b95fccd48c5
Manzana <73004527+ManzanaNaranja@users.noreply.github.com>
2024-06-04 07:51:38 -0700

    update credits page

--- scripts/credit.gd
+++ scripts/credit.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var coinpath = preload("res://Scenes/bouncycoin.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,3 +14,13 @@ func _process(delta):
 
 func _on_button_2_pressed():
 	get_tree().change_scene_to_file("res://Scenes/menu.tscn");
+	
+	
+func _input(ev):
+	if ev is InputEventMouseButton and ev.pressed:
+		var pos = ev.position
+		var coin = coinpath.instantiate()
+		coin.position = pos
+		get_parent().add_child(coin)
+		
+	
