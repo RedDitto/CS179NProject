365b8100ae9cf9baa232bec0c620a1587c72382c
Hirsch Chheda <66052427+Hirschheda@users.noreply.github.com>
2024-06-04 12:19:01 -0700

    Update RangedEnemy.gd

--- scripts/RangedEnemy.gd
+++ scripts/RangedEnemy.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-var health = 100
-var max_health = 100
+var health = 100/Global.mode
+var max_health = 100/Global.mode
 var speed = 40
 var range = 70
 var player_chase = false
